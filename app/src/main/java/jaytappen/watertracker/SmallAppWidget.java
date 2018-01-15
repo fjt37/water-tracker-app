c4866cfb0238c60ce79c139ff28b49284ed573ab
Jay Tappen <fjt37@cornell.edu>
2018-01-14 23:07:40 -0500

    Widget is clickable to open app

--- app/src/main/java/jaytappen/watertracker/SmallAppWidget.java
+++ app/src/main/java/jaytappen/watertracker/SmallAppWidget.java
@@ -1,8 +1,10 @@
 package jaytappen.watertracker;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 /**
@@ -20,8 +22,20 @@ public class SmallAppWidget extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.small_app_widget);
         views.setTextViewText(R.id.small_appwidget_text, text);
 
+        // Add a listener to the view to open the Main Activity if widget is pressed
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.small_appwidget, pendingIntent);
+
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+
+//        Intent intent = new Intent(context, ExampleActivity.class);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+//        // Get the layout for the App Widget and attach an on-click listener to the button
+//        RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.appwidget_provider_layout);
+//        views.setOnClickPendingIntent(R.id.button, pendingIntent);
+
     }
 
     @Override
